ddfd77751da97ac1f81529e0fae9fafb93e7f8df
JhonGiraldo <jhonferley18@gmail.com>
2019-08-16 11:03:07 -0500

    Se crea archivos para login

--- pages/conexion/mysql.php
+++ pages/conexion/mysql.php
@@ -1,5 +1,5 @@
 <?php	
-	$link = mysql_connect('localhost:3307', 'root', '1234')
+	$link = mysqli_connect('localhost', 'root', '')
 		or die('No se pudo conectar: ' . mysql_error());
-	mysql_select_db('db_evaluacion') or die('No se pudo seleccionar la base de datos');
+	mysqli_select_db($link,'db_evaluacion') or die('No se pudo seleccionar la base de datos');
 ?>
